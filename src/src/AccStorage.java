b217261a9587f7b05e086b19ba71ca95fbdb6b3a
Allen <112802362+ntlln@users.noreply.github.com>
2023-05-15 10:55:40 +0800

    Add files via upload

---
+++ src/src/AccStorage.java
@@ -0,0 +1,33 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Allen Caingcoy
+ */
+public class AccStorage {
+    private static final String fileloc = "G:" + File.separator + "Documents" + File.separator
+            + "NetBeans Projects" + File.separator + "login-validation" + File.separator + "src" + File.separator
+            + "rsc" + File.separator + "accs.txt";
+
+    public static String getfilepath() {
+        return fileloc;
+    }
+
+    public static void Accounts(String data) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileloc, true))) {
+            writer.write(data);
+            writer.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
